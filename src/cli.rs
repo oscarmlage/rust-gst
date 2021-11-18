5ea4eead9a8906b3779bd6005f9fe669b6cdbe2b
Óscar M. Lage <info@oscarmlage.com>
2021-11-18 20:53:13 +0100

    Add: New cli options related to stamps (--start, --stop and --update)

--- src/cli.rs
+++ src/cli.rs
@@ -38,6 +38,61 @@ pub fn build_cli() -> App<'static, 'static> {
             ),
         )
         .subcommand(
+            SubCommand::with_name("stamp")
+            .about("Manage stamps")
+            .arg(
+                Arg::with_name("start")
+                .short("s")
+                .long("start")
+                .help("Start a new stamp")
+                .takes_value(false),
+            )
+            .arg(
+                Arg::with_name("stop")
+                .short("S")
+                .long("stop")
+                .help("Stop last stamp")
+                .takes_value(false),
+            )
+            .arg(
+                Arg::with_name("update")
+                .short("u")
+                .long("update")
+                .help("Update last stamp")
+                .takes_value(false),
+            )
+            .arg(
+                Arg::with_name("task")
+                .short("t")
+                .long("task")
+                .value_name("TASK")
+                .help("Task related to the stamp")
+                .takes_value(true),
+            )
+            .arg(
+                Arg::with_name("description")
+                .short("d")
+                .long("description")
+                .value_name("DESCRIPTION")
+                .help("Task description")
+                .takes_value(true),
+            )
+            .arg(
+                Arg::with_name("dstart")
+                .long("dstart")
+                .value_name("DATE_START")
+                .help("Date stamp start")
+                .takes_value(true),
+            )
+            .arg(
+                Arg::with_name("dend")
+                .long("dend")
+                .value_name("DATE_END")
+                .help("Date stamp end")
+                .takes_value(true),
+            ),
+        )
+        .subcommand(
             SubCommand::with_name("tasks")
             .about("List of active tasks grouped by project")
             .arg(
